d9673e97712fed9e3bed3f06e1cda2d3529aa4b2
lhw1847 <lhw1847@gmail.com>
2022-05-17 17:20:27 +0900

    0517

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,19 +44,19 @@ class SupahScroll {
 
   animate() {
     this.scrollY += (window.scrollY - this.scrollY) * this.speed;
-    this.supahScroll.style.transform = `translate3d(0,${-this.scrollY}px,0)`;
+    this.supahScroll.style.transform = `translate3d(${-this.scrollY}px,0,0)`;
     this.raf = requestAnimationFrame(this.animate.bind(this));
   }
 
   scrollTo(y) {
-    window.scrollTo(0, y);
+    window.scrollTo(y, 0);
   }
 
   staticScrollTo(y) {
     cancelAnimationFrame(this.raf);
     this.scrollY = y;
-    window.scrollTo(0, y);
-    this.supahScroll.style.transform = `translate3d(0,${-y}px,0)`;
+    window.scrollTo(y, 0);
+    this.supahScroll.style.transform = `translate3d(${-y}px,0,0)`;
     this.play();
   }
 
@@ -66,6 +66,72 @@ class SupahScroll {
   }
 }
 
+function scroll(event) {
+  var section3Left = document
+    .querySelector("#section3")
+    .getBoundingClientRect().left;
+  let scrollTop =
+    window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+  parallaxWidth = wrap.getBoundingClientRect().width;
+
+  // document.body.style.height = parallaxWidth + "px";
+
+  let viewWidth = parallaxWidth - window.innerWidth;
+  let viewHeigth = parallaxWidth - window.innerHeight;
+  // let goLeft = scrollTop * (viewWidth / viewHeigth);
+
+  // gsap.to(wrap, {
+  //   left: -goLeft,
+  // });
+
+  // gsap.to(bgt, {
+  //   left: -goLeft / 10,
+  // });
+  // console.log(img01.getBoundingClientRect().left)
+  // console.log(document.querySelector("#section2 .text").getBoundingClientRect().left)
+  // if(img01.getBoundingClientRect().left <= 0){
+  //     console.log("first")
+  //     gsap.to(img01, {
+  //         left: 0,
+  //     })
+  // }
+  // if(document.querySelector("#section3").getBoundingClientRect().left >= 0 && img01.getBoundingClientRect().left >= 0) {
+  //     gsap.to(img01, {
+  //         left: goLeft,
+  //     })
+  // }
+  // gsap.to(img01, {
+  //         left: -goLeft + window.innerWidth / (100 / 60),
+  //     })
+
+  // if(img01.getBoundingClientRect().left >= 0 || 0 <= section3Left){
+  //     // console.log("first")
+  //     gsap.to(img01, {
+  //         left: -goLeft + window.innerWidth / (100 / 60),
+  //     })
+  // }
+
+  // var beforePosition = document.documentElement.scrollTop
+  // var afterPosition = document.documentElement.scrollTop;
+
+  // if (afterPosition > 1) {
+
+  // if(beforePosition < afterPosition ){
+  //     // 스크롤 위로
+  //     img01.querySelector(".img").style.transform = "translate(-100px , 0)";
+  // } else {
+  //     // 스크롤 아래로
+  //     img01.querySelector(".img").style.transform = "translate(100px , 0)";
+  // }
+
+  // } else {
+  //     // 평상 시
+  //     img01.querySelector(".img").style.transform = "translate(0 , 0)";
+  // }
+  requestAnimationFrame(scroll);
+}
+scroll();
+window.addEventListener("resize", scroll);
 /*------------------------------
      Initialize
      ------------------------------*/
